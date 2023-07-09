35cf0dfe65655a18e24c5a52bfdd7f7d09285598
Machine <68440743+Machinh@users.noreply.github.com>
2023-07-09 13:39:43 +0000

    Update MachinaServer.py

--- MachinaServer.py
+++ MachinaServer.py
@@ -10,7 +10,7 @@ class ServerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Server App")
-        self.root.geometry("300x200")
+        self.root.geometry("300x300")
 
         self.status_label = tk.Label(root, text="MachinaServer Inativo", fg="red")
         self.status_label.pack(pady=10)
@@ -21,17 +21,28 @@ class ServerApp:
         stop_button = tk.Button(root, text="Parar Servidor", command=self.stop_server)
         stop_button.pack(pady=10)
 
+        # Get the script's directory path
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # Load the image from the app's directory
+        image_path = os.path.join(script_dir, "404.png")
+        self.image = tk.PhotoImage(file=image_path)
+
+        # Create a label to display the image
+        self.image_label = tk.Label(root, image=self.image)
+        self.image_label.pack(pady=10)
+
         self.server_process = None
         self.check_server_thread = Thread(target=self.check_server_status)
         self.check_server_thread.start()
 
     def start_server(self):
         if self.server_process is None or self.server_process.poll() is not None:
-            # Obtém o diretório atual
-            current_dir = os.getcwd()
+            # Get the script's directory path
+            script_dir = os.path.dirname(os.path.abspath(__file__))
 
             # Inicia o servidor em um processo separado
-            self.server_process = subprocess.Popen(["python", "-m", "http.server", "9999"], cwd=current_dir)
+            self.server_process = subprocess.Popen(["python", "-m", "http.server", "9999"], cwd=script_dir)
             self.open_browser("http://localhost:9999/index.html")
 
     def stop_server(self):
